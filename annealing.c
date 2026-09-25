3498e60242edbae88f9a26b6a26285c2024e35f6
Vitor-Burgarelli <vcburgarelli@gmail.com>
2026-09-25 15:09:30 -0300

    Vetor inicial e instancias

--- annealing.c
+++ annealing.c
@@ -1,19 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <math.h>
 
 #define ESCALA 20 
 
-void inicia_vetor(){
+void inicia_vetor(int *vetor){
+
+    srand(time(NULL));
+
+    int k = rand() % (ESCALA + 1); 
+
+    for (int i = 0; i < k; i++) {
+        vetor[i] = 1;
+    }
+
+    for (int i = ESCALA - 1; i > 0; i--) {
+        int j = rand() % (i + 1);
+        int temp = vetor[i];
+        vetor[i] = vetor[j];
+        vetor[j] = temp;
+    }
+    
+
+    printf("Quantidade de 1s (k) = %d\n\n", k);
+    
+    printf("Vetor Inicial:\n");
+    for (int i = 0; i < ESCALA; i++) {
+        printf("%d ", vetor[i]);
+    }
+    printf("\n");
+
+    return;
+}
+
+void pega_clausulas(){
     
 }
 
 int main() { 
 
-    int vetor[ESCALA] = 0;
+    int vetor[ESCALA] = {0};
 
-    inicia_vetor(vetor, );
-
+    inicia_vetor(vetor);
+    pega_clausulas
 
     return 0;
 }
