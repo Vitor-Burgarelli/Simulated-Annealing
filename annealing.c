1233400cb56a32edb57b41a234422ea3cc41cbc8
Vitor-Burgarelli <vcburgarelli@gmail.com>
2026-09-25 13:14:06 -0300

    Commit Inicial

---
+++ annealing.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define ESCALA 20 
+
+void inicia_vetor(){
+    
+}
+
+int main() { 
+
+    int vetor[ESCALA] = 0;
+
+    inicia_vetor(vetor, );
+
+
+    return 0;
+}
